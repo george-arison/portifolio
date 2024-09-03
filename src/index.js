0dd0bdd6526e6faf25ca9d70a8420772fa6191e5
george-arison <119376441+george-arison@users.noreply.github.com>
2024-09-03 01:55:28 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector(".backToTop");
+window.addEventListener("scroll", function (event) {
+  if (window.scrollY == 0) {
+    botao.classList.remove("visible");
+  } else {
+    botao.classList.add("visible");
+  }
+});
